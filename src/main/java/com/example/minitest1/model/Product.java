364c65fc69cc55dd83392dfc07b3657997d37bc7
AnhDung95 <nguyendung2172013@gmail.com>
2022-09-19 16:07:23 +0700

    done add delete edit and 1/2 image

---
+++ src/main/java/com/example/minitest1/model/Product.java
@@ -0,0 +1,61 @@
+package com.example.minitest1.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String image;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, String image) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product[id=%d, name='%s', description='%s', image='%s']",id,name,description,image);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
